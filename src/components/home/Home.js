9952977f189295674d4bcfdffb9943c6c27253d5
Krushil Dhamelia <krushil1511995@gmail.com>
2020-03-14 12:18:38 +0530

    Use of local state for a stateful component Home

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 
 class Home extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      name : "Krushil"
+    }
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        name : "Krushil - Devil Kid"
+      })
+    }, 1000);
+  }
+
   render() {
     return (
       <div className="Home">
         <h1>
-            This is Home Component
+            This is Home Component owned by {this.state.name}
         </h1>
       </div>
     );
